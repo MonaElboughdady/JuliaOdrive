c6661e4e6347883b39305303548bf7634e1ec10e
MonaElboughdady <71582409+MonaElboughdady@users.noreply.github.com>
2021-06-29 15:18:32 +0200

    Add files via upload

--- using python Odrive library/usingOdrivePythonLibraryInJulia.jl
+++ using python Odrive library/usingOdrivePythonLibraryInJulia.jl
@@ -1,5 +1,5 @@
 
-ENV["PYTHON"] = raw"E:\anaconda3\python.exe"
+ENV["PYTHON"] = raw"E:\anaconda3\python.exe" 
 import Pkg
 Pkg.build("PyCall")
 using PyCall
@@ -7,7 +7,15 @@ odrive = pyimport("odrive")
 odrv0 = odrive.find_any()
 print(odrv0.vbus_voltage)
 py"""
-from odrive.enums import *
+from odrive.enums import * 
 """
-odrv0.axis0.requested_state = py"AXIS_STATE_FULL_CALIBRATION_SEQUENCE"
+py"""
+from odrive.utils import *
+"""
+odrv0.axis0.requested_state = py"AXIS_STATE_CLOSED_LOOP_CONTROL"
+for i=1:500
+    odrv0.axis0.controller.input_pos = sin(i*0.13)*0.3
+    sleep(0.01)
+end
 
+print(odrive.utils.dump_errors(odrv0))
